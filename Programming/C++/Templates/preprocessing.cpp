524c033ee0801a19525a4565d17e8fff8bf9596f
Pranav Chatur <pranavchatur@gmail.com>
2022-09-19 17:45:11 +0530

    Start using MassCode

--- Programming/C++/Templates/preprocessing.cpp
+++ Programming/C++/Templates/preprocessing.cpp
@@ -21,22 +21,25 @@ using namespace std;
 
 //Macros
 
+//*) Commonly used
+#define all(name) name.begin(),name.end()
+#define ll long long
+#define INF 1e9
+#define take_loop_input_in(name,size) for(int i=0;i<size;++i){cin>>name[i];}
+#define take_indexed_loop_input(name,size) for(int i=1;i<size+1;++i){int x;cin>>x;name.push_back(make_pair(x,i));}
+#define here cout<<"here\n"
+#define display(name) for(auto it: name){cout<<it<<" ";}cout<<"\n"
+#define what_is_here(name) cout<<#name<<": "<<name<<"\n"
+
 //*) general purpose
 #define FOR(i,start,upper_limit) for(i=start;i<upper_limit;++i)
 #define pb push_back
-#define all(name) name.begin(),name.end()
-#define ll long long
-#define inf 1e9
 
 //*) containers
 #define vi vector<int>
 #define vii vector < pair<int,int> >
 #define si set<int>
 #define msi multiset<int>
-
-//*) taking inputs in containers
-#define take_loop_input_in(name,size) for(int i=0;i<size;++i){cin>>name[i];}
-#define take_indexed_loop_input(name,size) for(int i=1;i<size+1;++i){int x;cin>>x;name.push_back(make_pair(x,i));}
 
 //*) Pair
 #define f first
@@ -45,11 +48,9 @@ using namespace std;
 #define p pair
 
 //*) debugging
-#define here cout<<"here\n"
 #define nl cout<<'\n'
-#define display(name) for(auto it: name){cout<<it<<" ";}cout<<"\n"
 #define display_paired(name) for(auto it: name){cout<<it.first<<"("<<it.second<<") ";}cout<<"\n"
-#define what_is_here(name) cout<<#name<<": "<<name<<"\n"
+#define display_2d(name, x, y) for(int i=0;i<x;++i){for(int j=0;j<y;++j){cout<<name[i][j]<<" ";}cout<<'\n';}
 
 #define free_cin cin.sync_with_stdio(false)
 #define free_cout cout.sync_with_stdio(false)
